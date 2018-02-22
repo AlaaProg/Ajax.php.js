b6050f01e16bfdc9eb685d4dadd377072680ce78
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-02-22 05:45:08 -0800

    Update index.php

--- index.php
+++ index.php
@@ -19,29 +19,15 @@
 		<script src="ajax.php.js" type="text/javascript" charset="utf-8"></script>
 
 		<script>
-
-			var req = request({
-
-					method:"POST",
-					url:"testAjax.php",
-					argv:"name=ali",
-					header:{
-						"Content-Type":"application/x-www-form-urlencoded",
-						"Content-type":"text/html; charset=utf-8;",
-						"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-					},
-					response:function(){
-
-						var ta = select('#TAjax');
-						ta.text('success');
-						ta.css('color',"green");
-
-					},
-					error:function(er){
-						alert(er);
-					}
-
+			// GET REQUEST //
+			Ajax({
+				url   :'testAjax.php',
+				argv  :"name=alaa",
+				done:(d)=>{
+					console.log(d.dom);
+				}
 			});
+	
 
 		</script>
 	</div>
