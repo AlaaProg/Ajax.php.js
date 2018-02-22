5b1dc8d9ec85271ef0dd3fc094620b553a727865
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-02-22 06:19:03 -0800

    Update index.php

--- index.php
+++ index.php
@@ -19,16 +19,34 @@
 		<script src="ajax.php.js" type="text/javascript" charset="utf-8"></script>
 
 		<script>
-			// GET REQUEST //
+			// select id #TAjax 
+			var ta = select('#TAjax')
 			Ajax({
-				url   :'testAjax.php',
-				argv  :"name=alaa",
-				done:(d)=>{
-					console.log(d.dom);
-				}
-			});
-	
 
+				url:"testAjax.php",
+				argv:"name=alaa",
+				start:()=>{
+					ta.text("Loading ...");
+					ta.css ("color","blue");
+				},
+				done:(d)=>{
+					
+					ta.text(" ");
+					ta.append(
+						// select class .content from testAjax.php 
+						select('.content',d.dom).element 
+						// and add To id #TAjax
+					);
+					ta.css ("color","#333");
+				},
+				error:(e)=>{
+					ta.text(e.status);
+					ta.css ("color","red");
+				}
+
+			});
+
+		</script>
 		</script>
 	</div>
 	</body>
