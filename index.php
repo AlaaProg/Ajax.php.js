3f7156cb7947c896f0bb280d07e4b7d5e61018ac
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-02-28 04:00:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,50 +5,21 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 	<title> Test Php & Ajax </title>
-	<link rel="stylesheet" href="style.css">
 </head>
 	<body>
-	<div class="body">
-		<h1> Test Php & Ajax </h1>
-		<div id="TAjax">
-			 
-		</div>
+	
+	<div class="content">
+		 
 
 
 
-		<script src="ajax.php.js" type="text/javascript" charset="utf-8"></script>
 
-		<script>
-			// select id #TAjax 
-			var ta = select('#TAjax')
-			Ajax({
-
-				url:"testAjax.php",
-				argv:"name=alaa",
-				start:()=>{
-					ta.text("Loading ...");
-					ta.css ("color","blue");
-				},
-				done:(d)=>{
-					
-					ta.text(" ");
-					ta.append(
-						// select class .content from testAjax.php 
-						select('.content',d.dom).element 
-						// and add To id #TAjax
-					);
-					ta.css ("color","#333");
-				},
-				error:(e)=>{
-					ta.text(e.status);
-					ta.css ("color","red");
-				}
-
-			});
-
-		</script>
-		</script>
 	</div>
+
+	
+	<script src="ajax.php.js" type="text/javascript" charset="utf-8"></script>
+
+	<script src="FAjax.js" type="text/javascript"></script>
 	</body>
 </html>
 
