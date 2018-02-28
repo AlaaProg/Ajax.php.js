1d695b19eb9357768003e41399546f2851287363
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-02-28 04:01:16 -0800

    Update ajax.php.js

--- ajax.php.js
+++ ajax.php.js
@@ -60,7 +60,6 @@ var Ajax = function (prm){
 	 		ajax.setRequestHeader(key,prm.header[key]);
 	 	});
  	}else{
- 		
  		if(method == "POST"){
 			ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 		}
@@ -77,43 +76,90 @@ var Ajax = function (prm){
 
 var select = function (id_class,dom=document){
 
-	var element = dom.querySelectorAll(id_class)[0];
-	
+	if (!id_class || id_class == ""){
+		var element = dom
+	}else{
+		var element = dom.querySelectorAll(id_class)[0];
+	}
 	return {
 
 		text: function (value=""){ 
 			if (value != ""){
-				return element.innerHTML = value
+				element.innerText = value
+			}else{ return element.innerText; }
+
+			return this
+		},
+		html: function (value=""){ 
+			if (value != ""){
+				element.innerHTML = value
 			}else{ return element.innerHTML; }
+
+			return this
 		},
 		
 		css: function(key,value=""){
 			if(value != ""){
 				element.style[key] = value;
 			}else{ return element.style[key]; }
+
+			return this
 		},
 		setattr: function(key,value){
 			element.setAttribute(key,value);
+			return this
 		},
 		getattr: function(key){
 			return element.getAttribute(key);
 		},
 		delattr: function(key){
 			element.removeAttribute(key);
+			return this
 		},
 		append: function(elem){
 			element.append(elem);
+			return this
 		},
-		remove: function(){
-			element.remove();
-		},
-		childOf:function(id_num=null){
-			if(id_num != null){
-				return select(id_class+" "+element.children[id_num].tagName.toLowerCase(),dom);
+		remove: function(NElem){
+
+			if (NElem){
+				element.removeChild(
+					select(NElem,element)
+					.element
+				);
+				return this
+			}else{
+				element.remove();
 			}
-			
 		},
-		get element(){ return element }
+		hide:function(){
+			element.style.display = 'none';
+
+			return this
+		},
+		show:function(){
+			element.style.display = '';
+
+			return this			
+		},
+		back:function(){
+			return select(
+				"",
+				dom=element.parentElement
+			);
+		},
+		value:function(){
+			return element.value
+		},
+		find:function(n){
+			return select(n,element);
+
+		},
+		event:function(NEvent,func){
+			element.addEventListener(NEvent,func);
+			return this;
+		},
+		get el(){ return element }
 	}
 	
 }
@@ -122,4 +168,69 @@ function h2dom(tagName,html){
 	var tag = document.createElement(tagName);
 	tag.innerHTML = html
 	return tag
+}
+
+
+class Request{
+
+	constructor(){
+		this.start = function(){  }
+		this.error = function(){  } 
+	}
+
+	_get(url,func){
+
+		Ajax({
+			url:url,
+			done:func,
+			error:this.error,
+			start:this.start
+		});
+
+	}
+
+
+	_post(url,argvs,func){
+
+		Ajax({
+			method:"POST",
+			url:url,
+			argv:argvs,
+			done:func,
+			error:this.error,
+			start:this.start
+
+		});
+
+	}
+
+	_put(url,argvs,func){
+
+		Ajax({
+			method:"PUT",
+			url:url,
+			argv:argvs,
+			done:func,
+			error:this.error,
+			start:this.start
+			
+		});
+
+	}
+
+	_delete(url,argvs,func){
+
+		Ajax({
+			method:"DELETE",
+			url:url,
+			argv:argvs,
+			done:func,
+			error:this.error,
+			start:this.start
+			
+		});
+
+	}
+
+
 }
